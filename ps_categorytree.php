c0f356cb9c93d2bdbed3c07e97d6a895d7c5f996
Maxime Biloé <maxime.biloe@gmail.com>
2016-08-16 10:40:13 +0200

    // Version updated to v1.0.1

--- ps_categorytree.php
+++ ps_categorytree.php
@@ -36,7 +36,7 @@ class Ps_CategoryTree extends Module implements WidgetInterface
     {
         $this->name = 'ps_categorytree';
         $this->tab = 'front_office_features';
-        $this->version = '1.0.0';
+        $this->version = '1.0.1';
         $this->author = 'PrestaShop';
 
         $this->bootstrap = true;
